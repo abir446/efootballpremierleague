1b9b80f96aef9646002bb3a94fe8a75dfa3f6fe1
Abir Dutta <abirdutta390@gmail.com>
2024-12-05 05:43:13 +0000

    init

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Home() {
+  return (
+    <div className="text-center items-center justify-center h-screen flex flex-col">
+      <div className="flex flex-col text-4xl font-extrabold">
+        <span> Efootball Premier League</span>
+        <span className="font-semibold text-2xl">Kolkata, India</span>
+      </div>
+    <span>Home of The Official Efootball League</span>
+    </div>
+  );
+}
