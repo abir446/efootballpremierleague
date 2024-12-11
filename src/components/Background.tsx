a7722ff3229c34d8999f67cd95af4cf71791e5e2
Souvik Rahut <souvikrahutofficial@gmail.com>
2024-12-11 22:36:58 +0530

    bg changed to auto scroll

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,21 +1,28 @@
 "use client";
 import React from "react";
 import { Vortex } from "../components/ui/vortex";
+
 interface Props {
   children: React.ReactNode;
 }
+
 export default function Background({ children }: Props) {
   return (
-    <div className="w-full h-screen overflow-hidden">
-      <Vortex
-        backgroundColor="black"
-        rangeY={800}
-        particleCount={300}
-        baseHue={120}
-        className="w-full h-full"
-      >
+    <div className="relative w-full h-screen">
+      {/* Vortex background */}
+      <div className="fixed inset-0 z-0">
+        <Vortex
+          backgroundColor="black"
+          rangeY={900}
+          particleCount={200}
+          baseHue={120}
+          className="w-full h-full"
+        />
+      </div>
+      {/* Content */}
+      <div className="relative z-10 w-full h-full overflow-y-auto">
         {children}
-      </Vortex>
+      </div>
     </div>
   );
 }
