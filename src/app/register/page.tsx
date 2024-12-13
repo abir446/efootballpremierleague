d07439cf86e5f4792666ca48248f1a46c7af0010
Souvik Rahut <souvikrahutofficial@gmail.com>
2024-12-13 17:46:06 +0530

    update in register

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -7,7 +7,7 @@ const Page = () => {
   return (
     <div className="text-white">
     
-      <div className="absolute h-72 top-0 left-0 right-0 bottom-44 my-auto ">
+      <div className="absolute h-36 md:h-72 top-56 md:top-28 left-0 right-0  my-auto ">
         <DotLottieReact
           src="/animations/404.lottie" // Ensure the file is in public/animations
           loop
@@ -17,8 +17,8 @@ const Page = () => {
         />
 
         <div className="flex flex-col justify-center items-center m-auto ">
-          <h1 className="text-[10vh] text-purple-500">Season Currently Closed!</h1>
-          <h3 className="text-[3vh]  max-w-[50%] text-center">
+          <h1 className="text-[5vh] text-center md:text-[10vh] text-orange-400">Season Currently Closed!</h1>
+          <h3 className="text-[2vh] md:text-[3vh] max-w-[70%]  md:max-w-[50%] text-center">
             The eFootball Premier League season is taking a break. Registration
             will reopen in <span className="text-lime-400">February 2025</span>. Stay tuned and get ready to showcase
             your skills in the next thrilling season!
